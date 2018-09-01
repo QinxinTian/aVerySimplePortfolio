22945cd61390d51ec51c74b9f9df615e5c276c81
QinxinTian <aptsubl@gmail.com>
2018-09-01 06:18:58 -0700

    for easy deploying.

---
+++ home.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Portfolio</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body>
+    <header class="header">
+        <div class="header_left">
+            <img src="img/logo.jpg" class="header_logo" alt="This is the logo">
+            <div class="header_right">
+                <h1 class="header_title">Qinxin Tian</h1>
+                <h2 class="header_second_title">Full Stack Web Developer</h2>
+            </div>
+        </div>
+    </header>
+
+        <div class="container">
+            <div class="cover">
+                <img src="img/html.jpg" class="cover_image" alt="html">
+            </div>
+
+            <div class="feature">
+                <h5 class="feature_title">Featured Work</h5>
+
+                <div class="feature_item">
+                    <article class="content">
+                        <img src="img/contribution.png" class="content_image" alt="contribution">
+                        <p class="content_title">Contribution</p>
+                        <a href="https://github.com/qinxintian" class="content_link">
+                            https://github.com/qinxintian
+                        </a>
+                    </article>
+                    <article class="content">
+                        <img src="img/datastructure.png" class="content_image" alt="datastructure">
+                        <h4 class="content_title">DataStructure</h4>
+                        <a href="https://github.com/qinxintian" class="content_link">
+                            https://github.com/qinxintian
+                        </a>
+                    </article>
+                    <article class="content">
+                        <img src="img/dovesmessenger.png" class="content_image" alt="dovesMessenger">
+                        <h4 class="content_title">A Online App</h4>
+                        <a href="https://github.com/qinxintian" class="content_link">
+                            https://github.com/qinxintian
+                        </a>
+                    </article>
+                </div>
+            </div>
+        </div>
+</body>
+
+</html>
